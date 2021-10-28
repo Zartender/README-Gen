142fd65671b7576177a92f2fd3c042a5e88bc541
Ian Greenblott <imgreenblott@gmail.com>
2021-10-28 19:18:41 -0400

    Updated js and added function for table of contents generator.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -10,23 +10,29 @@ function renderLicenseLink(license) {}
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
+function renderTable(contents){
+  if (contents === true){
+    return `## Table of Contents
+    If your README is long, add a table of contents to make it easy for users to find what they need.
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)` 
+  } else { return ""}
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   console.log(data)
   return `# ${data.title}
-  # <Your-Project-Title>
-  ## Description
+  ## Description: ${data.description}
   Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
   - What was your motivation?
   - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
   - What problem does it solve?
   - What did you learn?
-  ## Table of Contents (Optional)
-  If your README is long, add a table of contents to make it easy for users to find what they need.
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [Credits](#credits)
-  - [License](#license)
+ 
+  ${renderTable(data.contents)} 
+
   ## Installation
   What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
   ## Usage
